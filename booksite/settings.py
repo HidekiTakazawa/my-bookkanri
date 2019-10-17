c4f1bc057226e6a798612e4cd0beba9f4d6e3a96
HidekiTakazawa <ht0428a30@gmail.com>
2019-10-18 06:33:32 +0900

    PostgreSQL pythonanywhere local ok

--- booksite/settings.py
+++ booksite/settings.py
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'booksite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+###
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hidekibookdb',
+        'USER': 'hidekibu',
+        'PASSWORD': 'x271828abcd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
+###
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
